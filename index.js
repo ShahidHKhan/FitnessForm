0bc76399fcf8b2bc78013cc6aff418ede36850c2
Shahid Khan <107818138+ShahidHKhan@users.noreply.github.com>
2025-12-19 15:42:36 -0500

    Use environment variable for server port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 /* ---------- Middleware ---------- */
 app.use(express.json());
